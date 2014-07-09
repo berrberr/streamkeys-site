e00e49370021f8c25e5dc52ed0222d3c6c666a6c
Alex Gabriel <alexg@criticalmass.com>
2014-07-09 17:12:01 -0400

    Fixes to javascript.

--- _site/js/main.js
+++ _site/js/main.js
@@ -4,31 +4,22 @@ function getParameterByName(name) {
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
   var results = regex.exec(location.search);
   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+};
+
 
 $(function() {
-  var pageName = window.location.href.toString().split(window.location.host)[1].split('?')[0];
-  if(getParameterByName('installed')) $("#installed").toggle();
-  if(getParameterByName('updated')) $("#updated").toggle();
-  if(getParameterByName('sentmessage')) $("#sentmessage").toggle();
 
-  switch(pageName) {
-    case "/about.html":
-      $("#about").attr("class", "active");
-      break;
-    case "/help.html":
-      $("#help").attr("class", "active");
-      break;
-    case "/sites.html":
-      $("#sites").attr("class", "active");
-      break;
-    case "/contact.html":
-      $("#contact").attr("class", "active");
-      break;
-    default:
-      $("#navbar-main-brand").attr("class", "active-brand navbar-brand");
-      break;
-  }
+  $(".btn-install").click(function(e) {
+    console.log("installing...");
+    chrome.webstore.install(undefined,
+      function() {
+        console.log('success');
+      },
+      function(msg) {
+        console.log('failed, ' + msg);
+      }
+    );
+  });
 
   $("#showguide").click(function() {
     event.preventDefault();

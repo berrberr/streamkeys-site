152357f2ee03ec183b5943734161d8122a4d9a28
Alex Gabriel <alexg@criticalmass.com>
2014-08-25 17:02:17 -0400

    Add staging and live servers to sinatra contact form.

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -5,7 +5,7 @@ post '/contact' do
   #MANDRILL_APIKEY env var must be set
   m = Mandrill::API.new ENV['MANDRILL_APIKEY']
   request_origin = request.env['HTTP_ORIGIN']
-  accepted_origins = ['http://0.0.0.0:4000', 'http://localhost:4000']
+  accepted_origins = ['http://www.streamkeys.com', 'http://streamkeys.com', 'http://staging.streamkeys.com', 'http://0.0.0.0:4000', 'http://localhost:4000']
 
   #If the request is not coming from accept host reject it
   unless accepted_origins.include? request_origin

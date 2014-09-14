a7c4e9726446fd501f5bf41fc9f70c803f455e11
Alex Gabriel <alex@aegabriel.com>
2014-09-13 23:45:35 -0400

    Add grunt deploy tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,12 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-    aws: grunt.file.readJSON("aws-credentials.json"),
+    creds: grunt.file.readJSON("credentials.json"),
 
     s3: {
       options: {
-        accessKeyId: "<%= aws.accessKeyId %>",
-        secretAccessKey: "<%= aws.secretAccessKey %>",
+        accessKeyId: "<%= creds.accessKeyId %>",
+        secretAccessKey: "<%= creds.secretAccessKey %>",
         bucket: "streamkeys"
       },
       build: {
@@ -26,11 +26,37 @@ module.exports = function(grunt) {
           config: "_config-prod.yml,_config.yml"
         }
       }
+    },
+
+    environments: {
+      staging: {
+        options: {
+          host: "<%= creds.host %>",
+          username: "<%= creds.username %>",
+          password: "<%= creds.password %>",
+          deploy_path: "/var/www/streamkeys_staging",
+          local_path: "_site-production",
+          current_symlink: 'current'
+        }
+      },
+
+      production: {
+        options: {
+          host: "<%= creds.host %>",
+          username: "<%= creds.username %>",
+          password: "<%= creds.password %>",
+          deploy_path: "/var/www/streamkeys_live",
+          local_path: "_site-production"
+        }
+      }
     }
   });
 
-  grunt.registerTask("prod", ["jekyll:dist", "s3"]);
+  grunt.registerTask("build", ["jekyll:dist"]);
+  grunt.registerTask("stage-deploy", ["jekyll:dist", "s3", "ssh_deploy:staging"]);
+  grunt.registerTask("prod-deploy", ["jekyll:dist", "s3", "ssh_deploy:production"]);
 
-  grunt.loadNpmTasks('grunt-aws');
-  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks("grunt-aws");
+  grunt.loadNpmTasks("grunt-jekyll");
+  grunt.loadNpmTasks("grunt-ssh-deploy");
 };
